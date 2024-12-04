5510645a737ba21aa10e539610a7917d9ae277b0
Saskia von Mutius <saskia.vonmutius@gmail.com>
2024-12-04 22:58:55 +0100

    Weitere Optimierungen SCSS

--- resources/views/components/layout/eilinger.blade.php
+++ resources/views/components/layout/eilinger.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,10 +12,11 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>Eilinger Stiftung - @yield('title')</title>
-    <link rel="canonical" href="https://www.eilingerstiftung.ch/@yield('link')"/>
+    <link rel="canonical" href="https://www.eilingerstiftung.ch/@yield('link')" />
     <link rel="alternate" hreflang="de" href="https://www.eilingerstiftung.ch/de/@yield('link')" />
     <link rel="alternate" hreflang="en" href="https://www.eilingerstiftung.ch/en/@yield('link')" />
     <link rel="alternate" hreflang="x-default" href="https://www.eilingerstiftung.ch/@yield('link')" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/js/app.js'])
@@ -22,38 +24,41 @@
     @vite(['resources/sass/eilinger.scss'])
     @livewireStyles()
 
+
+
 </head>
 
 <body>
 
-<!-- ======= Header ======= -->
-@include('components.layout.header')
+    <!-- ======= Header ======= -->
+    @include('components.layout.header')
 
 
-<!-- ======= Hero Section ======= -->
-<section id="hero" class="d-flex align-items-center">
+    <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-2 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1"></div>
-            <div class="col-lg-8 order-1 order-lg-2 hero-img">
-                <img src="{{url('/images/logo_white.png')}}" class="img-fluid" alt="">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2 d-flex flex-column justify-content-center pt-4 pt-lg-0 order-2 order-lg-1"></div>
+                <div class="col-lg-8 order-1 order-lg-2 hero-img">
+                    <img src="{{ url('/images/logo_white.png') }}" class="img-fluid" alt="">
+                </div>
             </div>
         </div>
-    </div>
 
-</section><!-- End Hero -->
+    </section><!-- End Hero -->
 
-{{ $slot }}
+    {{ $slot }}
 
-@if (session()->has('success'))
-    <div class="position-absolute top-0 start-100 translate-middle p-3 mb-2 bg-success text-white">
-        <p>{{ session('success') }}</p>
-    </div>
-@endif
-<!-- ======= Footer ======= -->
-@include('components.layout.footer')
+    @if (session()->has('success'))
+        <div class="position-absolute top-0 start-100 translate-middle p-3 mb-2 bg-success text-white">
+            <p>{{ session('success') }}</p>
+        </div>
+    @endif
+    <!-- ======= Footer ======= -->
+    @include('components.layout.footer')
 
-@livewireScripts()
+    @livewireScripts()
 </body>
+
 </html>
