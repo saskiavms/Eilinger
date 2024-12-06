614561ee9dd54aec9e738c7ddc50d8b6282b59e1
Saskia von Mutius <saskia.vonmutius@gmail.com>
2024-12-06 21:34:40 +0100

    Weitere Korrekturen im JS

--- resources/js/eilinger.js
+++ resources/js/eilinger.js
@@ -157,40 +157,6 @@
 	});
 
 	/**
-	 * Porfolio isotope and filter
-	 */
-	window.addEventListener('load', () => {
-		let portfolioContainer = select('.portfolio-container');
-		if (portfolioContainer) {
-			let portfolioIsotope = new Isotope(portfolioContainer, {
-				itemSelector: '.portfolio-item',
-			});
-
-			let portfolioFilters = select('#portfolio-flters li', true);
-
-			on('click', '#portfolio-flters li', function (e) {
-				e.preventDefault();
-				portfolioFilters.forEach(function (el) {
-					el.classList.remove('filter-active');
-				});
-				this.classList.add('filter-active');
-
-				portfolioIsotope.arrange({
-					filter: this.getAttribute('data-filter')
-				});
-			}, true);
-		}
-
-	});
-
-	/**
-	 * Initiate portfolio lightbox
-	 */
-	const portfolioLightbox = GLightbox({
-		selector: '.portfolio-lightbox'
-	});
-
-	/**
 	 * Preloader
 	 */
 	let preloader = select('#preloader');
@@ -199,10 +165,5 @@
 			preloader.remove()
 		});
 	}
-
-	/**
-	 * Initiate Pure Counter
-	 */
-	new PureCounter();
 
 })()
