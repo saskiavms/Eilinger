629e890f5ec2dbd9ddccdc88e1dec5ffbfe53234
Saskia von Mutius <saskia.vonmutius@gmail.com>
2025-03-16 17:40:27 +0100

    Mail issue

--- app/Notifications/TwoFactorCode.php
+++ app/Notifications/TwoFactorCode.php
@@ -14,32 +14,11 @@ class TwoFactorCode extends Notification implements ShouldQueue
     use Queueable;
 
     /**
-     * The number of seconds the job can run before timing out.
-     *
-     * @var int
-     */
-    public $timeout = 120;
-
-    /**
-     * The number of times the job may be attempted.
-     *
-     * @var int
-     */
-    public $tries = 3;
-
-    /**
-     * The number of seconds to wait before retrying the job.
-     *
-     * @var int
-     */
-    public $backoff = 10;
-
-    /**
      * Create a new notification instance.
      */
     public function __construct()
     {
-        Log::info('TwoFactorCode notification instantiated');
+        //
     }
 
     /**
@@ -49,7 +28,6 @@ class TwoFactorCode extends Notification implements ShouldQueue
      */
     public function via(object $notifiable): array
     {
-        Log::info('TwoFactorCode via method called', ['user_id' => $notifiable->id]);
         return ['mail'];
     }
 
@@ -58,52 +36,13 @@ class TwoFactorCode extends Notification implements ShouldQueue
      */
     public function toMail(User $notifiable): MailMessage
     {
-        Log::info('TwoFactorCode toMail method called', [
-            'user_id' => $notifiable->id,
-            'code' => $notifiable->two_factor_code,
-            'locale' => app()->getLocale(),
-            'mail_config' => [
-                'driver' => config('mail.default'),
-                'host' => config('mail.mailers.smtp.host'),
-                'port' => config('mail.mailers.smtp.port'),
-                'from_address' => config('mail.from.address'),
-                'encryption' => config('mail.mailers.smtp.encryption')
-            ]
-        ]);
-
-        try {
-            $message = (new MailMessage())
-                ->subject(__('notify.two_factor'))
-                ->greeting(__('notify.greeting'))
-                ->line(__('notify.two_factor_line1', ['code' => $notifiable->two_factor_code]))
-                ->action(__('notify.two_factor_action'), route('verify.index', app()->getLocale()))
-                ->line(__('notify.two_factor_line2'))
-                ->line(__('notify.two_factor_line3'));
-
-            Log::info('TwoFactorCode email message built successfully', [
-                'to' => $notifiable->email,
-                'subject' => __('notify.two_factor')
-            ]);
-
-            return $message;
-        } catch (\Exception $e) {
-            Log::error('Error building 2FA email', [
-                'error' => $e->getMessage(),
-                'trace' => $e->getTraceAsString()
-            ]);
-            throw $e;
-        }
-    }
-
-    /**
-     * Handle a failed notification.
-     */
-    public function failed(\Exception $e)
-    {
-        Log::error('TwoFactorCode notification failed', [
-            'error' => $e->getMessage(),
-            'trace' => $e->getTraceAsString()
-        ]);
+        return (new MailMessage())
+            ->subject(__('notify.two_factor'))
+            ->greeting(__('notify.greeting'))
+            ->line(__('notify.two_factor_line1', ['code' => $notifiable->two_factor_code]))
+            ->action(__('notify.two_factor_action'), route('verify.index', app()->getLocale()))
+            ->line(__('notify.two_factor_line2'))
+            ->line(__('notify.two_factor_line3'));
     }
 
     /**
--- config/mail.php
+++ config/mail.php
@@ -34,6 +34,9 @@ return [
     */
 
     'mailers' => [
+        'sendgrid' => [
+            'transport' => 'sendgrid',
+        ],
         'smtp' => [
             'transport' => 'smtp',
             'url' => env('MAIL_URL'),
